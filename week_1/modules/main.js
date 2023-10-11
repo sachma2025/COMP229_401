ef2ccff8998e5341f472eadd3b32448df50dcfd9
Sachin Mapa <smapa@my.centennialcollege.ca>
2023-10-11 17:32:26 -0500

    new file:   week_1/classes/index.html 	new file:   week_1/classes/main.js     new file:   week_1/modules/index.html 	new file:   week_1/modules/lib.js 	new file:   week_1/modules/main.js 	new file:   week_1/modules/something.js 	new file:   week_1/modules/validator.js

---
+++ week_1/modules/main.js
@@ -0,0 +1,10 @@
+import { halfOf } from './lib.js';
+console.log(halfOf(84));
+
+import doSomething from './something.js';
+doSomething();
+
+import { flag, touch } from './validator.js';
+console.log(flag);
+touch();
+console.log(flag);
